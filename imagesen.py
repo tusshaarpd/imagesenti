c1477f6b77c8dfd331d067e7885d522521030ef3
tusshaarpd <134280025+tusshaarpd@users.noreply.github.com>
2025-01-31 20:24:46 +0530

    Update imagesen.py

--- imagesen.py
+++ imagesen.py
@@ -6,19 +6,14 @@ from deepface import DeepFace
 st.title("Image Emotion Detection App")
 st.write("Upload an image to analyze its emotion.")
 
-# File uploader for image input
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image", use_column_width=True)
     
-    # Emotion analysis on the uploaded image
     if st.button("Analyze Emotion"):
         # Perform emotion analysis using deepface
-        try:
-            result = DeepFace.analyze(image, actions=['emotion'], enforce_detection=False)
-            emotion = result[0]['dominant_emotion']  # Get the dominant emotion
-            st.write(f"### Detected Emotion: {emotion}")
-        except Exception as e:
-            st.write(f"Error analyzing the image: {e}")
+        result = DeepFace.analyze(image, actions=['emotion'], enforce_detection=False)
+        emotion = result[0]['dominant_emotion']  # Get the dominant emotion
+        st.write(f"### Detected Emotion: {emotion}")
